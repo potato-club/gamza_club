34680e09dba6a47ed08695f02ef9f7259e7a246f
donggyun <donggyunkim12@gmail.com>
2024-07-12 02:24:12 +0900

    chore: mainPage 더미데이터 삽입

--- src/app/(route)/(main)/_components/Projects.tsx
+++ src/app/(route)/(main)/_components/Projects.tsx
@@ -12,31 +12,31 @@ const Projects = () => {
   const dummy = [
     {
       id: 1,
-      cont: '1',
+      title: '1번 프로젝트',
     },
     {
       id: 2,
-      cont: '2',
+      title: '2번 프로젝트',
     },
     {
       id: 3,
-      cont: '3',
+      title: '3번 프로젝트',
     },
     {
       id: 4,
-      cont: '4',
+      title: '4번 프로젝트',
     },
     {
       id: 5,
-      cont: '5',
+      title: '5번 프로젝트',
     },
     {
       id: 6,
-      cont: '6',
+      title: '6번 프로젝트',
     },
     {
       id: 7,
-      cont: '7',
+      title: '7번 프로젝트',
     },
   ];
 
@@ -46,9 +46,15 @@ const Projects = () => {
         {Array.from({ length: dummy.length / 4 + 1 }).map((_, index) => (
           <CarouselItem key={index}>
             <div className="flex flex-wrap gap-6 h-[650px] border border-stone-200 bg-white rounded-3xl mt-6 py-6 px-10">
-              {dummy.slice(index * 4, index * 4 + 4).map((item, idx) => (
-                <ProjectItem key={idx} item={item} />
-              ))}
+              {dummy.length ? (
+                dummy
+                  .slice(index * 4, index * 4 + 4)
+                  .map((item, idx) => <ProjectItem key={idx} item={item} />)
+              ) : (
+                <span className="flex justify-center items-center font-bold w-full text-2xl">
+                  등록된 프로젝트가 없습니다.
+                </span>
+              )}
             </div>
           </CarouselItem>
         ))}
