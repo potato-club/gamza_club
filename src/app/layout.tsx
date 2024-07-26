2d1485f9174cf8812e593abedd7e3a92e54283ba
donggyun <donggyunkim12@gmail.com>
2024-07-26 11:48:50 +0900

    chore: nextjs 크롬익스텐션 충돌 warning 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={pretendard.className}>{children}</body>
+      <body className={pretendard.className} suppressHydrationWarning={true}>
+        {children}
+      </body>
     </html>
   );
 }
