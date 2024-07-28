805ddb35f953bacd7e6409a96afe89986357026b
ppacman <smwfkim2@naver.com>
2024-07-28 10:37:02 +0900

    feat. Card 모음 컴포넌트 생성

---
+++ src/app/(route)/signup/_components/CardNumber.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import FirstCard from "./FirstCard";
+import SecondCard from "./SecondCard";
+
+const CardNumber = () => {
+  return <div></div>;
+};
+
+export default CardNumber;
