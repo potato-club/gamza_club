11ece30fcc17847c83fba727d39f6e4d8da0c930
ppacman <smwfkim2@naver.com>
2024-07-28 10:35:40 +0900

    feat. 회원가입 페이지 메인 카드 컴포넌트 생성

--- src/app/(route)/signup/_components/MainCard.tsx
+++ src/app/(route)/signup/_components/MainCard.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Image from "next/image";
 import FirstCard from "./FirstCard";
+import SecondCard from "./SecondCard";
+import NextButton from "./NextButton";
+import useSignupState from "@/app/_store/signup/useSignupState";
 const MainCard = () => {
   return (
     <div className="w-[626px] h-[416px] rounded-xl border px-[40px] py-[36px] flex gap-[106px] bg-white	">
@@ -11,7 +14,10 @@ const MainCard = () => {
           1페이지
         </span>
       </div>
-      <FirstCard />
+      <div className="flex flex-col gap-[32px]">
+        <FirstCard />
+        <NextButton />
+      </div>
     </div>
   );
 };
