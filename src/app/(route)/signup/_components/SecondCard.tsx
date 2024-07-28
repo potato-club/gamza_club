88c1f223329599695267a1f73cc6b5510a0e726e
ppacman <smwfkim2@naver.com>
2024-07-28 10:34:56 +0900

    feat. 회원가입 페이지 카드2 컴포넌트 생성

---
+++ src/app/(route)/signup/_components/SecondCard.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const SecondCard = () => {
+  const inputStyle =
+    "w-[350px] h-[45px] rounded-xl border px-[16px] py-[12px] leading-none text-gray-500";
+
+  return (
+    <div className="flex flex-col gap-[21px]">
+      <input placeholder="이메일" className={`${inputStyle} mt-[70px]`} />
+      <input placeholder="비밀번호" className={inputStyle} />
+      <input placeholder="비밀번호 확인" className={inputStyle} />
+    </div>
+  );
+};
+
+export default SecondCard;
