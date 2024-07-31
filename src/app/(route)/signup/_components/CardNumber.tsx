f93ca1326504805d0e1bc67a0faf082b30c62ad0
ppacman <smwfkim2@naver.com>
2024-07-31 21:06:55 +0900

    feat. Funnel 패턴을 이용한 메인 컴포넌트 구성

--- src/app/(route)/signup/_components/CardNumber.tsx
+++ src/app/(route)/signup/_components/CardNumber.tsx
@@ -1,9 +1,61 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { useFunnel } from "@/app/_hooks/useFunnel";
 import FirstCard from "./FirstCard";
 import SecondCard from "./SecondCard";
+import ThirdCard from "./ThirdCard";
+
+type FormData = {
+  email: string;
+  password: string;
+  confirmPassword: string;
+  lastName: string;
+  firstName: string;
+  department: string;
+  studentID: string;
+};
 
 const CardNumber = () => {
-  return <div></div>;
+  const { Funnel, setStep } = useFunnel<"First" | "Second" | "Third">("First");
+
+  const [formData, setFormData] = useState<FormData>({
+    email: "",
+    password: "",
+    confirmPassword: "",
+    lastName: "",
+    firstName: "",
+    department: "",
+    studentID: "",
+  });
+
+  const handleSubmit = async () => {
+    console.log(formData);
+  };
+
+  return (
+    <div>
+      <Funnel>
+        <Funnel.Step name="First">
+          <FirstCard setStep={setStep} />
+        </Funnel.Step>
+        <Funnel.Step name="Second">
+          <SecondCard
+            setStep={setStep}
+            formData={formData}
+            setFormData={setFormData}
+          />
+        </Funnel.Step>
+        <Funnel.Step name="Third">
+          <ThirdCard
+            setStep={setStep}
+            formData={formData}
+            setFormData={setFormData}
+            handleSubmit={handleSubmit}
+          />
+        </Funnel.Step>
+      </Funnel>
+    </div>
+  );
 };
 
 export default CardNumber;
