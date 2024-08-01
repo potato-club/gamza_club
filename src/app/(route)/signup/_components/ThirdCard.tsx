f8eb4ce428651fc3015333cf6b9e65a1a1613990
ppacman <smwfkim2@naver.com>
2024-08-01 22:18:49 +0900

    fix. ThirdCard reaxct-hook-form 으로 변경

--- src/app/(route)/signup/_components/ThirdCard.tsx
+++ src/app/(route)/signup/_components/ThirdCard.tsx
@@ -1,84 +1,72 @@
-import React, { ChangeEvent } from "react";
+import React from "react";
+import { useForm } from "react-hook-form";
 import { Button } from "@/app/_components/ui/button";
 
-type ThirdCardProps = {
+interface FormData {
+  email: string;
+  password: string;
+  confirmPassword: string;
+  lastName: string;
+  firstName: string;
+  department: string;
+  studentID: string;
+}
+
+interface ThirdCardProps {
   setStep: (step: "First" | "Second" | "Third") => void;
-  formData: {
-    email: string;
-    password: string;
-    confirmPassword: string;
-    lastName: string;
-    firstName: string;
-    department: string;
-    studentID: string;
-  };
-  setFormData: React.Dispatch<
-    React.SetStateAction<{
-      email: string;
-      password: string;
-      confirmPassword: string;
-      lastName: string;
-      firstName: string;
-      department: string;
-      studentID: string;
-    }>
-  >;
-  handleSubmit: () => void;
-};
+  setFormData: React.Dispatch<React.SetStateAction<FormData>>;
+  handleSubmitForm: () => void;
+}
 
 const ThirdCard: React.FC<ThirdCardProps> = ({
   setStep,
-  formData,
   setFormData,
-  handleSubmit,
+  handleSubmitForm,
 }) => {
-  const handlePrev = () => {
-    setStep("Second");
-  };
+  const { register, handleSubmit } = useForm<FormData>();
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
+  const onSubmit = async (data: FormData) => {
     setFormData((prevData) => ({
       ...prevData,
-      [name]: value,
+      ...data,
     }));
+    handleSubmitForm();
+  };
+
+  const handlePrev = () => {
+    setStep("Second");
   };
 
   const inputStyle =
     "w-[350px] h-[45px] rounded-xl border px-[16px] py-[12px] leading-none text-gray-500";
 
   return (
-    <div className="flex flex-col gap-[32px]">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col gap-[32px]"
+    >
       <div className="flex flex-col gap-[21px]">
         <div className="flex mt-[70px] gap-[30px]">
           <input
-            name="lastName"
+            {...register("lastName")}
             placeholder="성"
             className="w-[160px] h-[45px] rounded-xl border px-[16px] py-[12px] leading-none text-gray-500"
-            value={formData.lastName}
-            onChange={handleChange}
           />
           <input
-            name="firstName"
+            {...register("firstName")}
             placeholder="이름"
             className="w-[160px] h-[45px] rounded-xl border px-[16px] py-[12px] leading-none text-gray-500"
-            value={formData.firstName}
-            onChange={handleChange}
           />
         </div>
         <input
-          name="department"
+          {...register("department")}
           placeholder="학과"
           className={inputStyle}
-          value={formData.department}
-          onChange={handleChange}
         />
         <input
-          name="studentID"
+          {...register("studentID")}
           placeholder="학번"
           className={inputStyle}
-          value={formData.studentID}
-          onChange={handleChange}
         />
       </div>
       <div className="flex items-center justify-end gap-[10px]">
@@ -89,16 +77,15 @@ const ThirdCard: React.FC<ThirdCardProps> = ({
         >
           이전
         </Button>
-
         <Button
+          type="submit"
           size="signup"
           className="bg-green-500 text-white hover:bg-white hover:text-green-500 hover:border-[2px] hover:border-green-500"
-          onClick={handleSubmit}
         >
           가입
         </Button>
       </div>
-    </div>
+    </form>
   );
 };
 
