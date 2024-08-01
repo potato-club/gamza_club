f3465d2800351b7cc91aece6c6ed9e63ce91b683
ppacman <smwfkim2@naver.com>
2024-08-01 14:29:51 +0900

    feat. 넘버별 카드 컴포너트 구현

--- src/app/(route)/signup/_components/FirstCard.tsx
+++ src/app/(route)/signup/_components/FirstCard.tsx
@@ -1,14 +1,42 @@
 import React from "react";
 import { Checkbox } from "@/app/_components/ui/checkbox";
-import NextButton from "./NextButton";
-const FirstCard = () => {
+import { Button } from "@/app/_components/ui/button";
+
+type FirstCardProps = {
+  setStep: (step: "First" | "Second" | "Third") => void;
+};
+
+const FirstCard: React.FC<FirstCardProps> = ({ setStep }) => {
+  const handleNext = () => {
+    setStep("Second");
+  };
   return (
-    <div className="flex flex-col  gap-[32px]">
-      <div className="w-[350px] h-[176px] rounded-xl border px-[16px] py-[12px] leading-none text-gray-500 mt-[70px]">
-        약관
-        <br />
-        1조.
-        <br />갑 김효성 을 김성훈 평생 충성을 다할것을 맹세한다
+    <div className="flex flex-col gap-[32px]">
+      <div className="flex flex-col  gap-[32px]">
+        <div className="w-[350px] h-[176px] rounded-xl border px-[16px] py-[12px] leading-none text-gray-500 mt-[70px]">
+          <div className="font-bold">약관</div>
+          <br />
+          1조.
+          <br />갑 김효성 을 김성훈 평생 충성을 다할것을 맹세
+        </div>
+      </div>
+      <div className={` flex items-center justify-between gap-[20px] `}>
+        <div className="flex items-center gap-2">
+          <Checkbox id="terms" />
+          <label
+            htmlFor="terms"
+            className=" decoration-green-500 hover:cursor-pointer"
+          >
+            회원가입 약관 동의
+          </label>
+        </div>
+        <Button
+          size="signup"
+          className="bg-green-500 text-white hover:bg-white hover:text-green-500 hover:border-[2px] hover:border-green-500"
+          onClick={handleNext}
+        >
+          다음
+        </Button>
       </div>
     </div>
   );
--- src/app/(route)/signup/_components/ThirdCard.tsx
+++ src/app/(route)/signup/_components/ThirdCard.tsx
@@ -1,14 +1,103 @@
-import React from "react";
+import React, { ChangeEvent } from "react";
+import { Button } from "@/app/_components/ui/button";
 
-const ThirdCard = () => {
+type ThirdCardProps = {
+  setStep: (step: "First" | "Second" | "Third") => void;
+  formData: {
+    email: string;
+    password: string;
+    confirmPassword: string;
+    lastName: string;
+    firstName: string;
+    department: string;
+    studentID: string;
+  };
+  setFormData: React.Dispatch<
+    React.SetStateAction<{
+      email: string;
+      password: string;
+      confirmPassword: string;
+      lastName: string;
+      firstName: string;
+      department: string;
+      studentID: string;
+    }>
+  >;
+  handleSubmit: () => void;
+};
+
+const ThirdCard: React.FC<ThirdCardProps> = ({
+  setStep,
+  formData,
+  setFormData,
+  handleSubmit,
+}) => {
+  const handlePrev = () => {
+    setStep("Second");
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
   const inputStyle =
     "w-[350px] h-[45px] rounded-xl border px-[16px] py-[12px] leading-none text-gray-500";
 
   return (
-    <div className="flex flex-col gap-[21px]">
-      <input placeholder="이메일" className={`${inputStyle} mt-[70px]`} />
-      <input placeholder="비밀번호" className={inputStyle} />
-      <input placeholder="비밀번호 확인" className={inputStyle} />
+    <div className="flex flex-col gap-[32px]">
+      <div className="flex flex-col gap-[21px]">
+        <div className="flex mt-[70px] gap-[30px]">
+          <input
+            name="lastName"
+            placeholder="성"
+            className="w-[160px] h-[45px] rounded-xl border px-[16px] py-[12px] leading-none text-gray-500"
+            value={formData.lastName}
+            onChange={handleChange}
+          />
+          <input
+            name="firstName"
+            placeholder="이름"
+            className="w-[160px] h-[45px] rounded-xl border px-[16px] py-[12px] leading-none text-gray-500"
+            value={formData.firstName}
+            onChange={handleChange}
+          />
+        </div>
+        <input
+          name="department"
+          placeholder="학과"
+          className={inputStyle}
+          value={formData.department}
+          onChange={handleChange}
+        />
+        <input
+          name="studentID"
+          placeholder="학번"
+          className={inputStyle}
+          value={formData.studentID}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="flex items-center justify-end gap-[10px]">
+        <Button
+          size="signup"
+          className="text-black border-[2px] border-gray-300 hover:bg-white hover:text-green-500 hover:border-[2px] hover:border-green-500"
+          onClick={handlePrev}
+        >
+          이전
+        </Button>
+
+        <Button
+          size="signup"
+          className="bg-green-500 text-white hover:bg-white hover:text-green-500 hover:border-[2px] hover:border-green-500"
+          onClick={handleSubmit}
+        >
+          가입
+        </Button>
+      </div>
     </div>
   );
 };
