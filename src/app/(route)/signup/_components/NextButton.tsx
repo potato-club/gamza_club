0e8239ea3cd72907f2912352c255106038eee6eb
ppacman <smwfkim2@naver.com>
2024-08-01 14:57:16 +0900

    feat. 공용 Button 컴포넌트 생성

--- src/app/(route)/signup/_components/NextButton.tsx
+++ src/app/(route)/signup/_components/NextButton.tsx
@@ -7,31 +7,19 @@ import useSignupState from "@/app/_store/signup/useSignupState";
 const NextButton = () => {
   const { inc, dec, count } = useSignupState();
 
-  const justify = count !== 0 ? "justify-end" : "justify-between";
-  const gap = count !== 0 ? "gap-[10px]" : "gap-[20px]";
+  const justify = count !== 1 ? "justify-end" : "justify-between";
+  const gap = count !== 1 ? "gap-[10px]" : "gap-[20px]";
 
   return (
-    <div className={` flex items-center ${justify} ${gap} `}>
-      {count === 0 ? (
-        <div className="flex items-center gap-2">
-          <Checkbox id="terms" />
-          <label
-            htmlFor="terms"
-            className=" decoration-green-500 hover:cursor-pointer"
-          >
-            회원가입 약관 동의
-          </label>
-        </div>
-      ) : null}
-      {count !== 0 ? (
-        <Button
-          size="signup"
-          className=" text-black border-[2px] border-gray-300 hover:bg-white hover:text-green-500 hover:border-[2px] hover:border-green-500"
-          onClick={dec}
-        >
-          이전
-        </Button>
-      ) : null}
+    <div className="flex items-center justify-end gap-[10px] ">
+      <Button
+        size="signup"
+        className=" text-black border-[2px] border-gray-300 hover:bg-white hover:text-green-500 hover:border-[2px] hover:border-green-500"
+        onClick={dec}
+      >
+        이전
+      </Button>
+
       <Button
         size="signup"
         className="bg-green-500 text-white hover:bg-white hover:text-green-500 hover:border-[2px] hover:border-green-500"
