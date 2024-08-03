146c7feda2c6cf7b9a9d7beefdd46c5e9aa6797d
donggyun <donggyunkim12@gmail.com>
2024-08-03 17:20:19 +0900

    fix: RHForm 컴포넌트 적용 및 post 페이지 Process Funnel hook 적용

--- src/app/(route)/post/_components/process/First.tsx
+++ src/app/(route)/post/_components/process/First.tsx
@@ -1,74 +1,45 @@
-import { CardButton, CardInput } from '@/app/_components/server/GamzaCard';
-import Link from 'next/link';
+import { CardButton } from '@/app/_components/server/GamzaCard';
 import React from 'react';
-import { useRouter } from 'next/navigation';
+import { RHFInput } from '@/app/_components/client/RHForm';
 import { useFormContext } from 'react-hook-form';
-import { PostSchemaType } from '@/app/_utils/validator/post';
-import { FormControl, FormField, FormItem } from '@/app/_components/ui/form';
-import { ErrorSpan } from '@/app/_components/client/FormWrapper';
 
-const First = () => {
-  const router = useRouter();
+const First = ({ onNext }: { onNext: () => void }) => {
   const {
+    control,
     formState: { errors },
     trigger,
-    control,
-  } = useFormContext<PostSchemaType>();
-
-  const next = async () => {
-    const titleValid = await trigger('title');
-    const describeValid = await trigger('describe');
-    if (titleValid && describeValid) {
-      router.push('/post?idx=2');
-    }
-  };
+  } = useFormContext();
 
   return (
     <div className="flex flex-col gap-y-5">
       <div className="flex flex-col gap-y-5 p-7 items-center">
-        <FormField
-          control={control}
+        <RHFInput
           name="title"
-          render={({ field }) => (
-            <div className="flex flex-col">
-              {errors.title?.message && (
-                <ErrorSpan message={errors.title.message} />
-              )}
-              <FormItem className="flex flex-row items-center w-full justify-between">
-                <FormControl>
-                  <CardInput
-                    stringSize="large"
-                    placeholder="프로젝트 이름"
-                    {...field}
-                  />
-                </FormControl>
-              </FormItem>
-            </div>
-          )}
-        />
-        <FormField
+          size="large"
+          placeholder="프로젝트 이름"
           control={control}
+          errors={errors}
+        />
+        <RHFInput
           name="describe"
-          render={({ field }) => (
-            <div className="flex flex-col">
-              {errors.describe?.message && (
-                <ErrorSpan message={errors.describe.message} />
-              )}
-              <FormItem className="flex flex-row items-center w-full justify-between">
-                <FormControl>
-                  <CardInput
-                    stringSize="large"
-                    placeholder="프로젝트 설명"
-                    {...field}
-                  />
-                </FormControl>
-              </FormItem>
-            </div>
-          )}
+          size="large"
+          placeholder="프로젝트 설명"
+          control={control}
+          errors={errors}
         />
       </div>
       <div className="flex justify-end gap-x-3">
-        <CardButton text="다음" color="green" onClick={next} />
+        <CardButton
+          text="다음"
+          color="green"
+          onClick={async () => {
+            const titleValid = await trigger('title');
+            const describeValid = await trigger('describe');
+            if (titleValid && describeValid) {
+              onNext();
+            }
+          }}
+        />
       </div>
     </div>
   );
--- src/app/(route)/post/page.tsx
+++ src/app/(route)/post/page.tsx
@@ -2,15 +2,10 @@ import GamzaCard from '@/app/_components/server/GamzaCard';
 import React from 'react';
 import Process from './_components/process';
 
-const page = ({ searchParams }: any) => {
-  const pageIdx = Number(searchParams.idx);
-
+const page = () => {
   return (
     <div className="flex-col gap-6">
-      <GamzaCard
-        title={`프로젝트 등록 페이지`}
-        content={<Process idx={pageIdx} />}
-      />
+      <GamzaCard title={`프로젝트 등록 페이지`} content={<Process />} />
     </div>
   );
 };
