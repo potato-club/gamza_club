c0220960ea0c7d7847f47fffe3471e150b731818
donggyun <donggyunkim12@gmail.com>
2024-08-08 01:14:09 +0900

    fix: repost 페이지 FormFunnel 이용한 Form 처리 수정

--- src/app/(route)/repost/_components/Content.tsx
+++ src/app/(route)/repost/_components/Content.tsx
@@ -1,63 +1,45 @@
 'use client';
 
-import Mutate from '@/app/_components/client/Mutate';
-import {
-  RHFFileInput,
-  RHFInput,
-  RHFWrapper,
-} from '@/app/_components/client/RHF';
+import { RHFFileInput, RHFInput } from '@/app/_components/client/RHF';
 import { CardButton } from '@/app/_components/server/GamzaCard';
-import { useSchemaForm } from '@/app/_hooks/useSchemaForm';
+import { useFormFunnel } from '@/app/_hooks/useFormFunnel';
 import { RepostSchema } from '@/app/_utils/validator/repost';
 import Link from 'next/link';
 import React from 'react';
 
 const Content = ({ id }: { id: number }) => {
-  const { form } = useSchemaForm(RepostSchema);
-  const {
-    control,
-    formState: { errors },
-    getValues,
-  } = form;
+  const { FormFunnel } = useFormFunnel('First');
 
   return (
-    <RHFWrapper
-      form={form}
-      onSubmit={form.handleSubmit((values) => {
-        const result = RepostSchema.safeParse(values);
-        if (result.success) alert('repost 제출 완료!');
-      })}
-    >
-      <div className="flex flex-col gap-y-5">
-        <div className="flex flex-col gap-y-6 items-center p-7">
-          <RHFFileInput
-            control={control}
-            errors={errors}
-            name="file"
-            placeholder="업로드"
-            label="애플리케이션 업로드"
-            fileName={getValues().file && getValues().file.name}
-          />
-          <RHFInput
-            control={control}
-            errors={errors}
-            name="port"
-            size="medium"
-            placeholder="포트 번호"
-            label="내부 포트"
-          />
+    <FormFunnel>
+      <FormFunnel.Step
+        name="First"
+        schema={RepostSchema}
+        onSubmit={(data) => console.log('repost submit 요청!', data)}
+      >
+        <div className="flex flex-col gap-y-5">
+          <div className="flex flex-col gap-y-6 items-center p-7">
+            <RHFFileInput
+              name="file"
+              placeholder="업로드"
+              label="애플리케이션 업로드"
+            />
+            <RHFInput
+              name="port"
+              size="medium"
+              placeholder="포트 번호"
+              label="내부 포트"
+            />
+          </div>
+          <div className="flex justify-end gap-x-3">
+            <Link href={'/'}>
+              <CardButton text="이전" />
+            </Link>
+            <CardButton text="요청" value="submit" color="green" />
+          </div>
         </div>
-        <div className="flex justify-end gap-x-3">
-          <Link href={'/'}>
-            <CardButton text="이전" />
-          </Link>
-          <Mutate
-            text="요청"
-            className="normal-button bg-[#36AE5A] text-white font-bold"
-          />
-        </div>
-      </div>
-    </RHFWrapper>
+      </FormFunnel.Step>
+    </FormFunnel>
   );
 };
 
