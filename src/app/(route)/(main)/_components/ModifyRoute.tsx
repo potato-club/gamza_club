bd98064af7c50315738b6467375d2b0931e8d062
donggyun <donggyunkim12@gmail.com>
2024-08-16 14:27:49 +0900

    feat: modify페이지 RHF 컴포넌트, useFormFunnel 훅 적용

--- src/app/(route)/(main)/_components/ModifyRoute.tsx
+++ src/app/(route)/(main)/_components/ModifyRoute.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 
 const ModifyRoute = ({ id }: { id: number }) => {
   return (
-    <Link href={`/project/fix?id=${id}`}>
+    <Link href={`/modify/${id}`}>
       <button className="normal-button bg-slate-200 p-3 hover:shadow-[0_0_0_2px_rgba(0,0,0)] ease-out duration-300 text-sm font-semibold">
         수정하기
       </button>
