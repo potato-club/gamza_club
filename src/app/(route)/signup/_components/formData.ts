ee0efecb99f4a2b6366c42c775c8113c94d30713
ppacman <smwfkim2@naver.com>
2024-08-21 15:41:22 +0900

    choir 사용하지 않는 컴포넌트 삭제

--- src/app/(route)/signup/_components/formData.ts
+++
@@ -1,9 +0,0 @@
-export interface FormData {
-  email: string;
-  password: string;
-  confirmPassword: string;
-  lastName: string;
-  firstName: string;
-  department: string;
-  studentID: string;
-}
--- src/app/(route)/signup/_components/page.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-const page = () => {
-  return <div>회원가입 페이지 뒤졌다 진짜</div>;
-};
-
-export default page;
