c7ec4cb444a90cde380a2c815ebb73afc2978f38
ppacman <smwfkim2@naver.com>
2024-08-21 15:35:29 +0900

    fix SecondCard FormFunnel 적용 수정

--- src/app/(route)/signup/_components/SecondCard.tsx
+++ src/app/(route)/signup/_components/SecondCard.tsx
@@ -1,77 +1,25 @@
 import React from "react";
-import { useForm } from "react-hook-form";
-import { Button } from "@/app/_components/ui/button";
-import { FormData } from "./formData";
-interface SecondCardProps {
-  setStep: (step: "First" | "Second" | "Third") => void;
-  setFormData: React.Dispatch<React.SetStateAction<FormData>>;
-  formData: FormData;
-}
+import { RHFInput } from "@/app/_components/client/RHF";
+import { CardButton } from "@/app/_components/server/GamzaCard";
 
-const SecondCard: React.FC<SecondCardProps> = ({
-  setStep,
-  setFormData,
-  formData,
-}) => {
-  const { register, handleSubmit, setValue } = useForm<FormData>({
-    defaultValues: formData,
-  });
-
-  const onSubmit = (data: FormData) => {
-    setFormData((prevData) => ({
-      ...prevData,
-      ...data,
-    }));
-    setStep("Third");
-  };
-
-  const handlePrev = () => {
-    setStep("First");
-  };
-
+const SecondCard = () => {
   const inputStyle =
     "w-[350px] h-[45px] rounded-xl border px-[16px] py-[12px] leading-none text-gray-500";
 
   return (
     <div className="flex flex-col gap-[32px]">
-      <div className="flex flex-col gap-[21px]">
-        <input
-          {...register("email")}
-          placeholder="이메일"
-          className={`${inputStyle} mt-[70px]`}
-          defaultValue={formData.email}
-        />
-        <input
-          {...register("password")}
-          type="password"
-          placeholder="비밀번호"
-          className={inputStyle}
-          defaultValue={formData.password}
-        />
-        <input
-          {...register("confirmPassword")}
-          type="password"
+      <div className="flex flex-col gap-[21px] mt-[70px]">
+        <RHFInput name="email" size="large" placeholder="이메일" />
+        <RHFInput name="password" size="large" placeholder="비밀번호" />
+        <RHFInput
+          name="passwordConfirm"
+          size="large"
           placeholder="비밀번호 확인"
-          className={inputStyle}
-          defaultValue={formData.confirmPassword}
         />
       </div>
       <div className="flex items-center justify-end gap-[10px]">
-        <Button
-          size="signup"
-          className="text-black border-[2px] border-gray-300 hover:bg-white hover:text-green-500 hover:border-[2px] hover:border-green-500"
-          onClick={handlePrev}
-        >
-          이전
-        </Button>
-        <Button
-          type="submit"
-          size="signup"
-          className="bg-green-500 text-white hover:bg-white hover:text-green-500 hover:border-[2px] hover:border-green-500"
-          onClick={handleSubmit(onSubmit)}
-        >
-          다음
-        </Button>
+        <CardButton text="이전" value="prev" />
+        <CardButton text="다음" color="green" value="next" />
       </div>
     </div>
   );
