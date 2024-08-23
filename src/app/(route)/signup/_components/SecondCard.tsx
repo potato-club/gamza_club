3c32b9d8fa724419a58cc06839a1cab8723b848f
ppacman <smwfkim2@naver.com>
2024-08-23 13:24:46 +0900

    feat. 비밀번호 동일성 검사 로직 작성

--- src/app/(route)/signup/_components/SecondCard.tsx
+++ src/app/(route)/signup/_components/SecondCard.tsx
@@ -1,25 +1,41 @@
 import React from "react";
-import { RHFInput } from "@/app/_components/client/RHF";
+import { RHFInput, RHFSameValueChecker } from "@/app/_components/client/RHF";
 import { CardButton } from "@/app/_components/server/GamzaCard";
+import { useFormContext, useWatch } from "react-hook-form";
 
 const SecondCard = () => {
-  const inputStyle =
-    "w-[350px] h-[45px] rounded-xl border px-[16px] py-[12px] leading-none text-gray-500";
+  const { control } = useFormContext();
 
+  const password = useWatch({ control, name: "password" });
+  const passwordConfirm = useWatch({ control, name: "passwordConfirm" });
+
+  const passwordsMatch = password === passwordConfirm;
   return (
     <div className="flex flex-col gap-[32px]">
       <div className="flex flex-col gap-[21px] mt-[70px]">
-        <RHFInput name="email" size="large" placeholder="이메일" />
-        <RHFInput name="password" size="large" placeholder="비밀번호" />
+        <RHFInput name="email" size="large" placeholder="이메일" type="email" />
+        <RHFInput
+          name="password"
+          size="large"
+          placeholder="비밀번호"
+          type="password"
+        />
+        {!passwordsMatch && <div>비밀번호가 일치해야합니다.</div>}
         <RHFInput
           name="passwordConfirm"
           size="large"
           placeholder="비밀번호 확인"
+          type="password"
         />
       </div>
       <div className="flex items-center justify-end gap-[10px]">
         <CardButton text="이전" value="prev" />
-        <CardButton text="다음" color="green" value="next" />
+        <CardButton
+          text="다음"
+          color="green"
+          value="next"
+          disabled={!passwordsMatch}
+        />
       </div>
     </div>
   );
