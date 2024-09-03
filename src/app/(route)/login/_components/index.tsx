2a6d0c9a2d8109e9d95cb1394107b08fb7759b2e
ppacman <smwfkim2@naver.com>
2024-09-03 13:03:44 +0900

    feat. 로그인 페이지 생성 및 api 초기 연동

---
+++ src/app/(route)/login/_components/index.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import React from "react";
+import { useForm, FormProvider } from "react-hook-form";
+import { RHFInput } from "@/app/_components/client/RHF";
+import { CardButton } from "@/app/_components/server/GamzaCard";
+import axios from "axios";
+
+const Login = () => {
+  const methods = useForm();
+  const { handleSubmit } = methods;
+
+  const onSubmit = async (data: any) => {
+    try {
+      const response = await axios.post("http://3.34.207.58:8080/user/login", {
+        email: data.email,
+        password: data.password,
+      });
+
+      if (response.status === 200) {
+        alert("로그인에 성공했습니다!");
+      }
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.error(
+          "Failed to login:",
+          error.response?.data || error.message
+        );
+        alert("로그인에 실패했습니다. 다시 시도해주세요.");
+      } else {
+        console.error("Error:", error);
+      }
+    }
+  };
+
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="w-[350px] h-[176px]">
+          <RHFInput name="email" placeholder="이메일" size="large" />
+          <RHFInput name="password" placeholder="비밀번호" size="large" />
+          <CardButton
+            text="로그인"
+            color="green"
+            value="submit"
+            type="submit"
+          />
+        </div>
+      </form>
+    </FormProvider>
+  );
+};
+
+export default Login;
--- src/app/(route)/login/page.tsx
+++ src/app/(route)/login/page.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-const page = () => {
-  return <div>login page</div>;
+import React from "react";
+import GamzaCard from "@/app/_components/server/GamzaCard";
+import Login from "./_components";
+const index = () => {
+  return <GamzaCard title={"로그인"} content={<Login />} />;
 };
 
-export default page;
+export default index;
