4820721dc508f93077b06ea6a67e242d48e4b2a6
ppacman <smwfkim2@naver.com>
2024-09-05 16:10:36 +0900

    feat. 로그인 페이지 ui 수정

--- src/app/(route)/login/_components/index.tsx
+++ src/app/(route)/login/_components/index.tsx
@@ -34,20 +34,24 @@ const Login = () => {
   };
 
   return (
-    <FormProvider {...methods}>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="w-[350px] h-[176px]">
-          <RHFInput name="email" placeholder="이메일" size="large" />
-          <RHFInput name="password" placeholder="비밀번호" size="large" />
-          <CardButton
-            text="로그인"
-            color="green"
-            value="submit"
-            type="submit"
-          />
-        </div>
-      </form>
-    </FormProvider>
+    <div className="w-full flex justify-center items-center">
+      <FormProvider {...methods}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="w-[350px] h-[176px] flex flex-col gap-4">
+            <RHFInput name="email" placeholder="이메일" size="large" />
+            <RHFInput name="password" placeholder="비밀번호" size="large" />
+            <div className={` flex items-center justify-end gap-[20px] `}>
+              <CardButton
+                text="로그인"
+                color="green"
+                value="submit"
+                type="submit"
+              />
+            </div>
+          </div>
+        </form>
+      </FormProvider>
+    </div>
   );
 };
 
