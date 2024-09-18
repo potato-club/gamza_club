f7ab5a57190a8c94c74bf500018a2c50cd88a65e
ppacman <smwfkim2@naver.com>
2024-09-18 20:54:36 +0900

    feat. 회원가입 완료후 로그인 페이지로 라우팅

--- src/app/(route)/signup/_components/CardNumber.tsx
+++ src/app/(route)/signup/_components/CardNumber.tsx
@@ -6,10 +6,13 @@ import ThirdCard from "./ThirdCard";
 import { useFormFunnel } from "@/app/_hooks/useFormFunnel";
 import { SignUpSchema } from "@/app/_utils/validator/signup";
 import axios from "axios";
+import { useRouter } from "next/navigation";
 const CardNumber = () => {
   const { FormFunnel, setStep } = useFormFunnel<"First" | "Second" | "Third">(
     "First"
   );
+  const router = useRouter();
+
   const handleSubmit = async (data: any) => {
     try {
       const response = await axios.post("http://3.34.207.58:8080/user/signup", {
@@ -20,6 +23,8 @@ const CardNumber = () => {
         major: data.major,
         studentId: data.studentNumber,
       });
+      alert("회원가입이 완료되었습니다!");
+      router.push("/login");
     } catch (error) {
       if (axios.isAxiosError(error)) {
         console.error(
