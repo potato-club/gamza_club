9d4fd2f7aeb4decbe03a08efa4b28a52222e4e5a
ppacman <smwfkim2@naver.com>
2024-09-20 13:17:55 +0900

    feat. QueryProvider 초기 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,23 @@
-import type { Metadata } from 'next';
-import './_styles/globals.css';
-import localFont from 'next/font/local';
+import type { Metadata } from "next";
+import "./_styles/globals.css";
+import localFont from "next/font/local";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const pretendard = localFont({
-  src: '../../public/fonts/PretendardVariable.woff2',
-  display: 'swap',
-  weight: '100 200 300 400 500 600 700 800 900',
-  variable: '--font-pretendard',
+  src: "../../public/fonts/PretendardVariable.woff2",
+  display: "swap",
+  weight: "100 200 300 400 500 600 700 800 900",
+  variable: "--font-pretendard",
 });
 
 export const metadata: Metadata = {
-  title: 'Gamza',
-  description: 'Hansei Gamza club website',
+  title: "Gamza",
+  description: "Hansei Gamza club website",
   icons: {
-    icon: '/icon.ico',
+    icon: "/icon.ico",
   },
 };
+const queryClient = new QueryClient();
 
 export default function RootLayout({
   children,
@@ -23,10 +25,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={pretendard.className} suppressHydrationWarning={true}>
-        {children}
-      </body>
-    </html>
+    <QueryClientProvider client={queryClient}>
+      <html lang="en">
+        <body className={pretendard.className} suppressHydrationWarning={true}>
+          {children}
+        </body>
+      </html>
+    </QueryClientProvider>
   );
 }
