12acf8e7c5702922d83c4ea149ac39b1a6b80eae
ppacman <smwfkim2@naver.com>
2024-10-01 20:44:35 +0900

    feat. 폴더명 변경

--- src/app/_hooks/useGetApproceList.tsx
+++
@@ -1,6 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { getApproveList } from "@/app/_utils/getApproveList";
-
-// export const useApproveList = () => {
-//   return useQuery(["approveList"], getApproveList, {});
-// };
--- src/app/_utils/getApproveList.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const getApproveList = async () => {
-  const res = await axios.get(
-    `${process.env.NEXT_PUBLIC_API_URL}/admin/user/approve/list`
-  );
-  return res.data;
-};
