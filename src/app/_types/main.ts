59edc386eddf6e0e79ce884430d06be7100adf88
donggyun <donggyunkim12@gmail.com>
2024-10-11 16:53:33 +0900

    fix: main 프로젝트 Item state 타입 변경

--- src/app/_types/main.ts
+++ src/app/_types/main.ts
@@ -7,7 +7,7 @@ export type Item = {
   id: number;
   name: string;
   description: string;
-  state: ['PLAN', 'PROGRESS', 'DONE'];
+  state: 'PLAN' | 'PROGRESS' | 'DONE';
   startedDate: string;
   endedDate: string;
 };
