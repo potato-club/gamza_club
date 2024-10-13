c00d697ac9bf656b88c1251a91123942f1eb0bdc
donggyun <donggyunkim12@gmail.com>
2024-10-13 18:12:45 +0900

    feat: 404 페이지 홈으로가기 버튼 추가

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,6 +5,11 @@ const NotFoundPage = () => {
     <div className="flex flex-col gap-y-8 justify-center items-center bg-background min-h-screen">
       <img src="/Logo.svg" width={100} height={100} />
       <h1 className="font-bold text-4xl">요청한 페이지를 찾을 수 없습니다.</h1>
+      <button className="max-w-fit h-11 normal-button px-6 bg-[#36AE5A]">
+        <a href="http://localhost:3000" className="font-bold text-white">
+          홈으로 가기
+        </a>
+      </button>
     </div>
   );
 };
