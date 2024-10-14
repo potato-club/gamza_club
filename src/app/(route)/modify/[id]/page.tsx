2d08647423ecad2d9156ea6bf0b315f8e3ef76ae
donggyun <donggyunkim12@gmail.com>
2024-10-14 11:28:46 +0900

    feat: cookie값을 이용한 동적 server fetch 구현

--- src/app/(route)/modify/[id]/page.tsx
+++ src/app/(route)/modify/[id]/page.tsx
@@ -1,4 +1,5 @@
 import GamzaCard from '@/app/_components/server/GamzaCard';
+import { cookies } from 'next/headers';
 import { notFound } from 'next/navigation';
 import React, { Suspense, use } from 'react';
 import Content from '../_components/Content';
@@ -22,14 +23,15 @@ const page = ({ params }: any) => {
 export default page;
 
 const getdata = async (id: number) => {
+  const accessToken = cookies().get('accessToken')?.value;
+
   try {
     const res = await fetch(
       `${process.env.NEXT_PUBLIC_API_URL}/project/${id}`,
       {
         next: { revalidate: 10 },
         headers: {
-          Authorization:
-            'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI3NzlmYzc0NzhkMGQ3YzM2YTdiMzk3OWMxZDUwMjA5MGE1ZDliMjhlZjBiZmFmZjhiOWEyZGRlZDhjNTg0ODg3YjM0ZjBkYTk1MDEzZmY5Mzc4NzNlMzg4YzE0MjQ5YjgiLCJpYXQiOjE3Mjg4MTAxNzAsImV4cCI6MTcyODgxMzc3MH0.JgaEosboFq4ARHtUlWPhxmZqah_-1nbQT6ERzlONTnBv6Ek0Mgmv3GkCr-hOnIkvVBVcEyqhZrPqWS3nxph4bQ',
+          Authorization: `Bearer ${accessToken}`,
         },
       }
     );
