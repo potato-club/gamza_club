1ce1e69921a69dde72504f62000af2a6e51f3195
ppacman <smwfkim2@naver.com>
2024-11-13 16:53:46 +0900

    feat. 로그인 hook 쿠키 설정

--- src/app/_hooks/query/useLogin.tsx
+++ src/app/_hooks/query/useLogin.tsx
@@ -2,22 +2,35 @@ import { useMutation } from "@tanstack/react-query";
 import { login, LoginData } from "@/app/_utils/api/login";
 import { useRouter } from "next/navigation";
 import { useCookies } from "react-cookie";
+
 export const useLogin = () => {
   const router = useRouter();
-  const [cookies, setCookie, removeCookie] = useCookies(["accessToken"]);
+  const [, setCookie] = useCookies(["accessToken", "refreshToken"]);
+
   const { mutate, isError, error } = useMutation({
     mutationKey: ["login"],
     mutationFn: (data: LoginData) => login(data),
     onSuccess: (response) => {
-      setCookie("accessToken", response.headers.authorization, {
-        httpOnly: true,
-        maxAge: 3600,
-      });
+      const accessToken = response.headers.authorization;
+      const refreshToken = response.headers.refreshtoken;
+
+      if (accessToken) {
+        setCookie("accessToken", accessToken, {
+          httpOnly: true,
+        });
+      }
+
+      if (refreshToken) {
+        setCookie("refreshToken", refreshToken, {
+          httpOnly: true,
+        });
+      }
+
       alert("로그인에 성공했습니다.");
-      console.log(cookies);
       router.push("/");
     },
     onError: (error) => {
+      console.error("로그인 실패:", error);
       alert("로그인에 실패했습니다.");
     },
   });
--- src/app/_utils/api/login.ts
+++ src/app/_utils/api/login.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import apiClient from "./axiosClient";
+import axiosClient from "./axiosClient";
 
 export interface LoginData {
   email: string;
