3e519ee0b4ec11970d81b14fd2caed90b3bab1f5
ppacman <smwfkim2@naver.com>
2024-11-22 14:31:12 +0900

    fix 로그인 훅 수정

--- src/app/_hooks/query/useLogin.tsx
+++ src/app/_hooks/query/useLogin.tsx
@@ -1,31 +1,14 @@
 import { useMutation } from "@tanstack/react-query";
 import { login, LoginData } from "@/app/_utils/api/login";
 import { useRouter } from "next/navigation";
-import { useCookies } from "react-cookie";
-
+import { AxiosResponse } from "axios";
 export const useLogin = () => {
   const router = useRouter();
-  const [, setCookie] = useCookies(["accessToken", "refreshToken"]);
 
   const { mutate, isError, error } = useMutation({
     mutationKey: ["login"],
     mutationFn: (data: LoginData) => login(data),
     onSuccess: (response) => {
-      const accessToken = response.headers.authorization;
-      const refreshToken = response.headers.refreshtoken;
-
-      if (accessToken) {
-        setCookie("accessToken", accessToken, {
-          httpOnly: true,
-        });
-      }
-
-      if (refreshToken) {
-        setCookie("refreshToken", refreshToken, {
-          httpOnly: true,
-        });
-      }
-
       alert("로그인에 성공했습니다.");
       router.push("/");
     },
