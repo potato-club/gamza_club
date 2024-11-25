04dd624b1319b0811af21a7d69dd25f8b09cdd8d
ppacman <smwfkim2@naver.com>
2024-11-25 20:37:32 +0900

    chore. api Route 변수명 수정

--- src/app/api/reissue/route.ts
+++ src/app/api/reissue/route.ts
@@ -1,7 +1,7 @@
-import { NextResponse } from 'next/server';
-import axios from 'axios';
-import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
+import { NextResponse } from "next/server";
+import axios from "axios";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export async function GET() {
   try {
@@ -9,7 +9,7 @@ export async function GET() {
       `${process.env.NEXT_PUBLIC_API_URL}/user/reissue`,
       {
         headers: {
-          Authorization: cookies().get('refresh')?.value,
+          Authorization: cookies().get("refresh")?.value,
         },
       }
     );
@@ -19,17 +19,17 @@ export async function GET() {
     // 클라이언트로 토큰 반환
     return NextResponse.json({ authorization }, { status: 200 });
   } catch (error) {
-    console.error('Error during authentication:', error);
+    console.error("Error during authentication:", error);
 
     // 에러 응답 처리
     if (axios.isAxiosError(error)) {
       const status = error.response?.status || 500;
-      const message = error.response?.data?.error || 'Failed to authenticate';
+      const message = error.response?.data?.error || "Failed to authenticate";
       return NextResponse.json({ error: message }, { status });
     }
 
     return NextResponse.json(
-      { error: 'Internal Server Error' },
+      { error: "Internal Server Error" },
       { status: 500 }
     );
   }
