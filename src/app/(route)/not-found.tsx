44199a3a8ac592de3da8233541e0b60f59986991
donggyun <donggyunkim12@gmail.com>
2024-11-28 17:40:19 +0900

    chore(app): layout, not-found 컴포넌트 폴더 이동

--- src/app/(route)/not-found.tsx
+++ src/app/(route)/not-found.tsx
@@ -1,9 +1,10 @@
+import Image from 'next/image';
 import React from 'react';
 
 const NotFoundPage = () => {
   return (
     <div className="flex flex-col gap-y-8 justify-center items-center bg-background min-h-screen">
-      <img src="/Logo.svg" width={100} height={100} />
+      <Image src="/Logo.svg" width={100} height={100} alt="gamza-logo" />
       <h1 className="font-bold text-4xl">요청한 페이지를 찾을 수 없습니다.</h1>
       <button className="max-w-fit h-11 normal-button px-6 bg-[#36AE5A]">
         <a href="http://localhost:3000" className="font-bold text-white">
