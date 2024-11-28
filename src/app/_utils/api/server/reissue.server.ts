7ee310a826f9a1cf0d0ffa04507036d41aea6b47
donggyun <donggyunkim12@gmail.com>
2024-11-28 15:31:36 +0900

    fix(utils): reissue.server.ts 캐싱 방식 time-based 로 변경

--- src/app/_utils/api/server/reissue.server.ts
+++ src/app/_utils/api/server/reissue.server.ts
@@ -1,18 +1,15 @@
-import axios from 'axios';
 import { cookies } from 'next/headers';
 
 export const getAtFromRt = async () => {
   const refresh = cookies().get('refreshToken')?.value;
 
   try {
-    const res = await axios.get(
-      `${process.env.NEXT_PUBLIC_API_URL}/user/reissue`,
-      {
-        headers: {
-          refreshToken: `Bearer ${refresh}`,
-        },
-      }
-    );
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/reissue`, {
+      headers: {
+        refreshToken: `Bearer ${refresh}`,
+      },
+      next: { revalidate: 3600 },
+    });
 
     return res.headers;
   } catch (err) {
