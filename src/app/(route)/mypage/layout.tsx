8bd173ed0482c1635ebf96a0c57322869b1e42ef
donggyun <donggyunkim12@gmail.com>
2024-11-28 15:13:59 +0900

    style(Mypage): layout header, main 태그 css 조정

--- src/app/(route)/mypage/layout.tsx
+++ src/app/(route)/mypage/layout.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 export default function MypageLayout({
   children,
 }: {
@@ -5,8 +7,12 @@ export default function MypageLayout({
 }) {
   return (
     <div className="bg-background min-h-screen flex flex-col gap-y-4 justify-center items-center">
-      <img src="/Logo.svg" width={48} height={48} />
-      {children}
+      <header>
+        <Image src="/Logo.svg" width={48} height={48} alt="gamza" />
+      </header>
+      <main className="w-full h-[520px] flex justify-center items-center">
+        {children}
+      </main>
     </div>
   );
 }
