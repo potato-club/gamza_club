5797badbefae54803ef77c574b58893f92b80294
ppacman <smwfkim2@naver.com>
2025-01-02 18:50:53 +0900

    fix. approveList get api 수정 적용

--- src/app/(route)/admin/user/UserList.tsx
+++ src/app/(route)/admin/user/UserList.tsx
@@ -1,10 +1,10 @@
 "use client";
 import React from "react";
 import UserItem from "../_components/UserItem";
-import { useApproveList } from "@/app/_hooks/query/useGetApproceList";
+import { useGetApproveList } from "@/app/_hooks/query/useGetApproceList";
 
 const UserList = () => {
-  const { data, isLoading } = useApproveList();
+  const { data } = useGetApproveList();
 
   return (
     <div className="flex flex-col gap-y-4 w-[1010px] h-[520px] border border-stone-200 bg-white rounded-lg py-10 px-8 overflow-auto">
