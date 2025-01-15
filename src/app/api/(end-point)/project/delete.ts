1664d4740f1a796a86acccc79c5acb66c21544ad
donggyun <donggyunkim12@gmail.com>
2025-01-15 23:57:32 +0900

    fix: apiClient 파일명 변경에 따른 import 경로 변경

--- src/app/api/(end-point)/project/delete.ts
+++ src/app/api/(end-point)/project/delete.ts
@@ -1,4 +1,4 @@
-import apiClient from '@/app/_utils/api/axiosClient';
+import apiClient from '@/app/_utils/api/apiClient';
 
 export const deleteProject = async (projectId: number) => {
   const res = await apiClient.delete(`/project/user/list/${projectId}`);
